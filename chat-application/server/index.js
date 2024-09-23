6ee8adb241f567aafabc9087ce266583eb7e17c6
Karthik0484 <karthikdeepshika484@gmail.com>
2024-09-23 14:16:13 +0530

    added database

--- chat-application/server/index.js
+++ chat-application/server/index.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
-
 dotenv.config();
 
 const app = express();
@@ -11,11 +10,10 @@ const port = process.env.PORT || 3001;
 const databaseURL = process.env.DATABASE_URL;
 
 
+
 const server = app.listen(port, () => {
     console.log(`server is running at http://localhost:${port}`);
 });
-
-mongoose
-.connnect(databaseURL)
-.then(() => console.log("Database Connection Successfull"))
+mongoose.connect(databaseURL)
+.then(()=>console.log("DB connection successful"))
 .catch((err) => console.log(err.message));
