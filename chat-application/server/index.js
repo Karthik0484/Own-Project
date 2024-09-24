21d793d0c2d12d3f717a2543423e74bf6ca37e34
Karthik0484 <karthikdeepshika484@gmail.com>
2024-09-24 06:44:37 +0530

    Auth model

--- chat-application/server/index.js
+++ chat-application/server/index.js
@@ -3,13 +3,22 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 const databaseURL = process.env.DATABASE_URL;
 
+app.use(cors({
+    origin: [process.env.ORIGIN],
+    methods: ["GET","POST","PUT","PATCH","DELETE"],
+    credentials: true,
+})
+);
 
+app.use(cookieParser());
+app.use(express.json());
 
 const server = app.listen(port, () => {
     console.log(`server is running at http://localhost:${port}`);
