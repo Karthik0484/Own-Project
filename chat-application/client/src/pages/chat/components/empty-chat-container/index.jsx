a60fb11c0e5266e97f8b488b45ca5769478be682
Karthik0484 <karthikdeepshika484@gmail.com>
2025-01-27 11:52:39 +0530

    DirectMessage-model Completed

--- chat-application/client/src/pages/chat/components/empty-chat-container/index.jsx
+++ chat-application/client/src/pages/chat/components/empty-chat-container/index.jsx
@@ -2,7 +2,8 @@ import Lottie from "react-lottie";
 import { animationDefaultOptions } from "@/lib/utils";
 
 const EmptyChatContainer = () => {
-  return ( <div className="flex-1 md:bg-[#1c1d25] md:flex flex-col justify-center items-center hidden duration-1000 transition-all">EmptyChatContainer
+  return ( 
+  <div className="flex-1 md:bg-[#1c1d25] md:flex flex-col justify-center items-center hidden duration-1000 transition-all">EmptyChatContainer
   <Lottie 
    isClickToPauseDisabled={true}
    height={200}
