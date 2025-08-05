beee3cc4abddec041123a8eecdb0200d7a1a9209
Karthik0484 <karthikdeepshika484@gmail.com>
2025-08-05 21:34:11 +0530

    solved gopd Error

--- chat-application/client/src/lib/api-client.js
+++ chat-application/client/src/lib/api-client.js
@@ -1,10 +1,22 @@
 import axios from "axios";
 import { HOST } from "@/utils/constants";
 
-
-
-
 export const apiClient = axios.create({
     baseURL: HOST,
+    withCredentials: true,
 });
+
+// Add request interceptor to include auth token
+apiClient.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('authToken');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
